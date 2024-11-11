01e2607df49908d2d22b488cdfa3fdb1fe9155dc
LabTS <wheelhousemark+labts@gmail.com>
2024-11-11 10:02:24 +0000

    provided code

---
+++ sse_cw6_nov_2024.sql
@@ -0,0 +1,31 @@
+-- Q1 returns (country,capital,population)
+
+;
+
+-- Q2 returns (continent,country)
+
+;
+
+-- Q3 returns (organization_name,country_name))
+
+;
+
+-- Q4 returns (country,neighbour,length)
+
+;
+
+-- Q5 returns (name,type)
+
+;
+
+-- Q6 returns (country,population)
+
+;
+
+-- Q7 returns (country,no_neighbours,border_length)
+
+;
+
+-- Q8 returns (organization,no_members,population)
+
+;
